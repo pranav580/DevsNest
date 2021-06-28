851edbedbdff3d42f2bece2af66499b9287f17bf
pranav580 <65352773+pranav580@users.noreply.github.com>
2021-06-28 22:42:40 +0530

    quiz updated

--- Devsnest_THA/Day 11/app.js
+++ Devsnest_THA/Day 11/app.js
@@ -1,7 +1,7 @@
 const quiz = document.querySelectorAll('.quiz');
 const center = document.querySelector('h2');
 const op = document.querySelectorAll('.op');
-console.log(op);
+console.log(op.length);
 var i=0;
 const questions = ["","Which famous con-artist's life is the film Catch Me If You Can based on?",
                     "How many stars does the U.S flag have?",
@@ -15,17 +15,23 @@ const options = [[],
                 ];
 const ans =[0,2,3,1];
 
+function correct(){
+   
+}
+
 const next = ()=> {
-    console.log(options);
+    console.log(options.length);
     if(i==0){
         quiz[i].classList.replace('n','p');
         i=i+1;
         quiz[i].classList.remove('p');
     }
     center.innerHTML = questions[i];
-    for (let j = 0; j < options.length; j++){
-        op[j].innerHTML = options[i][j];
-    }
+    op[0].innerHTML = options[i][0];
+    op[1].innerHTML = options[i][1];
+    op[2].innerHTML = options[i][2];
+    op[3].innerHTML = options[i][3];
+    correct();
     console.log(i);
     i=i+1;
     if(i==5){
@@ -34,19 +40,20 @@ const next = ()=> {
 }
 const prev = ()=> {  
     center.innerHTML = questions[i];
-    for (let j = 0; j < options; j++) {
-        op.innerHTML = options[i];
-    }
+    op[0].innerHTML = options[i][0];
+    op[1].innerHTML = options[i][1];
+    op[2].innerHTML = options[i][2];
+    op[3].innerHTML = options[i][3];
     console.log(i);
     i=i-1;
-    if(i==0){
+    if(i==1){
         i=5;
     }
 }
 
 document.querySelector('.next').addEventListener('click',function(){
     next();
-
+    
 });
 
 document.querySelector('.prev').addEventListener('click',function(){
