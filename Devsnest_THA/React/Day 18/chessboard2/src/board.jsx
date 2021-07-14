5793686f8dcf5866c42a1edfe9029e24854b7656
pranav580 <65352773+pranav580@users.noreply.github.com>
2021-07-14 23:21:38 +0530

    Day 18

--- Devsnest_THA/React/Day 18/chessboard2/src/board.jsx
+++ Devsnest_THA/React/Day 18/chessboard2/src/board.jsx
@@ -1,25 +1,31 @@
-const Box = ()=>{
-    return(<div class="box"> </div>)
-}
-var arr =[1,2,3,4,5,6,7,8];
+const Box = (props)=>{
+    return(<div className="box" style={{backgroundColor: props.color}}>
 
-const BoxRow=()=>{
-    return( <div class="boxrow">
-        {
-        arr.map(item=>
-            <Box/>
-        ) 
-        }
     </div>)
 }
+
 
 const Board=()=>{
-    return(<div class="board">
-         {
-        arr.map(item=>
-            <BoxRow/>
-        ) 
-        }
-    </div>)
+    return (
+        <div className="board">
+            {
+            [...Array(64)].map((element,index)=>{
+                let COLOR="red";
+                if((Math.floor(index/8)%2===0 && index%2===0) || (Math.floor(index/8)%2!==0 && index%2!==0))
+                {
+                    COLOR="black";
+                }
+                else
+                {
+                    COLOR="white"
+                }
+
+                return(
+                <Box color={COLOR}/>
+                )
+            })
+            }
+        </div>
+    )
 }
 export default Board;
