570961fd1fe5a0b1f1061450bdf20e5c6c96f103
Pranav Bhangale <pranavbhangale580@gmail.com>
2021-07-15 14:21:45 +0530

    Update Components.jsx

--- Devsnest_THA/React/Day 19/buttons/src/Components.jsx
+++ Devsnest_THA/React/Day 19/buttons/src/Components.jsx
@@ -2,9 +2,7 @@ import { useState } from 'react';
 
 const Btn = ()=>{
     const [number,setValue]=useState(0)
-    return(
-        <button className="btn" onClick={()=>setValue(number+1)}>{number}</button>
-    )
+    return( <button className="btn" onClick={()=>setValue(number+1)}>{number}</button> )
 }
 
 export default Btn;
