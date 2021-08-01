000e61d7881d65f1c30a73818d1937fc14144b48
pranav580 <65352773+pranav580@users.noreply.github.com>
2021-08-01 14:25:07 +0530

    Login page

--- Devsnest_THA/React/Day 23/my_app/src/Components.jsx
+++ Devsnest_THA/React/Day 23/my_app/src/Components.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-
+import { useState } from "react";
 export default function App() {
+    const [log, setLog] = useState(false);
+    const [Login, setLogin] = useState("Login");
+
   return (
     <Router>
       <div>
@@ -9,8 +12,25 @@ export default function App() {
           <div className="navbar">
               <Link to="/">Home</Link>
               <Link to="/about">About</Link>
-              <Link to="/profile">Profile</Link>
-              <Link to="/dashboard">Dashboard</Link>
+              {log === true ? (
+            <>
+              <Link to="/profile">
+                Profile
+              </Link>
+              <Link to="/dashboard">
+                Dashboard
+              </Link>
+            </>
+          ) : (
+            <>
+              <Link to="/">
+                Profile
+              </Link>
+              <Link to="/">
+                Dashboard
+              </Link>
+            </>
+          )}
           </div>
         </nav>
 
@@ -18,16 +38,14 @@ export default function App() {
           <Route path="/about">
             <About />
           </Route>
-          if(state == true){
-            <Route path="/profile">
-                <Profile />
-            </Route>
-          }
+          <Route path="/profile">
+            <Profile />
+           </Route>
           <Route path="/dashboard">
             <Dashboard />
           </Route>
-          <Route path="/">
-            <Home />
+          <Route >
+            <Home path="/" Login={Login} setLogin={setLogin} setLog={setLog} log={log}/>
           </Route>
         </Switch>
       </div>
@@ -35,11 +53,21 @@ export default function App() {
   );
 }
 
-function Home() {
+function Home({Login , setLogin , setLog, log}) {
   return( 
         <React.Fragment>
             <h2>Home</h2>
             <h2>If not logged in cant access, Profile & Dashboard</h2>
+            <button onClick={()=>{
+                    // log ==="false" ? (
+                        setLogin("Logout");
+                        setLog(true);
+                    // ):
+                    //     setLogin("Login");
+                    //     setLog("false");
+                    // )
+            }}>{Login}</button>
+
         </React.Fragment>
         );
 }
