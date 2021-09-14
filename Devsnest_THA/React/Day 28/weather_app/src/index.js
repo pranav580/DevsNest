ee373c602ea8edac7d513c902d476e9c42aa7f48
pranav580 <65352773+pranav580@users.noreply.github.com>
2021-09-15 02:36:45 +0530

    update

--- Devsnest_THA/React/Day 28/weather_app/src/index.js
+++ Devsnest_THA/React/Day 28/weather_app/src/index.js
@@ -4,6 +4,7 @@ import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
+    <div className="nav justify-content-center">Weather App</div>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
